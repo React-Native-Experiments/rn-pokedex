9333c07c1da4a67ae9f8fafa86c1ae4d57d3a666
BearingMe <gomes.bruno201@gmail.com>
2026-03-04 17:47:02 -0300

    feat: add usePokemon hook for fetching individual Pokemon details

--- src/api/pokemon/queries.ts
+++ src/api/pokemon/queries.ts
@@ -1,6 +1,6 @@
 import http from "@/lib/http";
-import { PokemonPage } from "@/types/pokemon";
-import { useInfiniteQuery } from "@tanstack/react-query";
+import { PokemonDetails, PokemonPage } from "@/types/pokemon";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
 
 export function useInfinitePokemons() {
   return useInfiniteQuery({
@@ -13,5 +13,16 @@ export function useInfinitePokemons() {
     initialPageParam: "https://pokeapi.co/api/v2/pokemon?offset=0&limit=20",
     getNextPageParam: ({ next }) => next,
     getPreviousPageParam: ({ previous }) => previous,
+  });
+}
+
+export function usePokemon(id: string) {
+  return useQuery({
+    queryKey: ["pokemon", "details", id],
+    queryFn: async () => {
+      const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+      const response = await http.get<PokemonDetails>(url);
+      return response.data;
+    },
   });
 }
