af091caa5757d2cb22144d5369b0a2df228ed3d5
BearingMe <gomes.bruno201@gmail.com>
2026-03-04 12:50:00 -0300

    refactor: remove unused Pokemon query and update type in useInfinitePokemons function

--- src/api/pokemon/queries.ts
+++ src/api/pokemon/queries.ts
@@ -1,32 +1,12 @@
 import http from "@/lib/http";
-import { Pokemon } from "@/types/pokemon";
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
-
-interface PokemonQueryResponse {
-  count: number;
-  next: string | null;
-  previous: string | null;
-  results: Pokemon[];
-}
-
-export function usePokemons() {
-  return useQuery({
-    queryKey: ["pokemon"],
-    queryFn: async () => {
-      const response = await http.get<PokemonQueryResponse>(
-        "https://pokeapi.co/api/v2/pokemon",
-      );
-
-      return response.data;
-    },
-  });
-}
+import { PokemonPage } from "@/types/pokemon";
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 export function useInfinitePokemons() {
   return useInfiniteQuery({
     queryKey: ["pokemon"],
     queryFn: async ({ pageParam }) => {
-      const response = await http.get<PokemonQueryResponse>(pageParam);
+      const response = await http.get<PokemonPage>(pageParam);
       return response.data;
     },
 
