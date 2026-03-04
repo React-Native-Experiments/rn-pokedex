272bfd0cbcc7d92c28973f9e0050a1fbd1c71a09
BearingMe <gomes.bruno201@gmail.com>
2026-03-04 11:01:51 -0300

    feat: implement Http class with GET method for API requests

---
+++ src/lib/http.ts
@@ -0,0 +1,29 @@
+class Http {
+  async get<T = unknown>(
+    url: string,
+    params?: Record<string, string | number | boolean>,
+  ) {
+    const urlObj = new URL(url);
+
+    if (params) {
+      const paramsStr = Object.fromEntries(
+        Object.entries(params).map(([k, v]) => [k, v.toString()]),
+      );
+
+      urlObj.search = new URLSearchParams(paramsStr).toString();
+    }
+
+    const response = await fetch(urlObj);
+
+    if (!response.ok) {
+      throw new Error(response.status.toString());
+    }
+
+    return {
+      ...response,
+      data: (await response.json()) as T,
+    };
+  }
+}
+
+export default new Http();
