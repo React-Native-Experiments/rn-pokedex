e202832190ee6c01c7b812b476cdd0669a1b2237
BearingMe <gomes.bruno201@gmail.com>
2026-03-05 00:18:44 -0300

    feat: add TYPE_COLORS constant and create theme colors for consistent styling

--- src/constants/pokemon.ts
+++ src/constants/pokemon.ts
@@ -15,3 +15,24 @@ export const STAT_COLORS: Record<string, string> = {
   "special-defense": "#A7DB8D",
   speed: "#FA92B2",
 };
+
+export const TYPE_COLORS = {
+  fire: "#F08030",
+  normal: "#A8A878",
+  water: "#6890F0",
+  electric: "#F8D030",
+  grass: "#78C850",
+  ice: "#98D8D8",
+  fighting: "#C03028",
+  poison: "#A040A0",
+  ground: "#E0C068",
+  flying: "#A890F0",
+  psychic: "#F85888",
+  bug: "#A8B820",
+  rock: "#B8A038",
+  ghost: "#705898",
+  dragon: "#7038F8",
+  dark: "#705848",
+  steel: "#B8B8D0",
+  fairy: "#EE99AC",
+};
