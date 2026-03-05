ca23c4f1aadec5f5866ecb1b3f4e61177c5282e3
BearingMe <gomes.bruno201@gmail.com>
2026-03-05 16:11:53 -0300

    feat: add PokemonHero component for displaying Pokemon details

---
+++ src/components/pokemon/PokemonHero.tsx
@@ -0,0 +1,41 @@
+import { Image as ExpoImage } from "expo-image";
+import { Text, View } from "react-native";
+
+interface PokemonHeroProps {
+  name: string;
+  types: string[];
+  artwork?: string | null;
+  backgroundColor: string;
+}
+
+export function PokemonHero(props: PokemonHeroProps) {
+  const { name, types, artwork, backgroundColor } = props;
+
+  const renderPokemonType = (type: string, idx: number) => (
+    <View className="bg-white/30 rounded-full px-3 py-1" key={idx}>
+      <Text className={"uppercase text-xs text-white font-black"}>{type}</Text>
+    </View>
+  );
+
+  return (
+    <View
+      className="pt-14 items-center justify-center rounded-b-[40] gap-3 bg-white "
+      style={{ backgroundColor }}
+    >
+      <Text className="capitalize text-white text-3xl font-bold">{name}</Text>
+
+      <View className="flex-row gap-2">
+        {types.map((t, idx) => renderPokemonType(t, idx))}
+      </View>
+
+      {artwork && (
+        <ExpoImage
+          source={artwork}
+          style={{ width: 200, height: 200 }}
+          contentFit="contain"
+          transition={200}
+        />
+      )}
+    </View>
+  );
+}
