a12ffc6d1ecdc96ccceed3de415e39e4ff90a392
BearingMe <gomes.bruno201@gmail.com>
2026-03-06 16:05:43 -0300

    fix: improve search functionality by debouncing input in usePokemonSearch hook

--- src/hooks/usePokemonSearch.ts
+++ src/hooks/usePokemonSearch.ts
@@ -1,13 +1,20 @@
 import { PokemonPage } from "@/types/pokemon";
 import { useMemo, useState } from "react";
+import { useDebounceValue } from "usehooks-ts";
 
 export function usePokemonSearch(data: PokemonPage[]) {
   const [search, setSearch] = useState("");
+  const [debouncedSearch] = useDebounceValue(search, 300);
+
   const pages = data.flatMap((p) => p.results);
 
   const filtered = useMemo(
-    () => pages.filter((p) => !search || p.name.includes(search.toLowerCase())),
-    [pages, search],
+    () =>
+      pages.filter(
+        (p) =>
+          !debouncedSearch || p.name.includes(debouncedSearch.toLowerCase()),
+      ),
+    [pages, debouncedSearch],
   );
 
   return { filtered, search, setSearch };
